6e09fa53965331a4ae3ce2cb439dfb67bf328173
YuHsiang <b6100111524@hotmail.com>
2018-01-05 20:35:07 +0800

    add admin restaurants route by resources

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "restaurants#index"
   
-  # 後台路由設定 建立admin管理權限
+  # 後台路由設定 建立admin管理權限 (使用前置字元定義)
   namespace :admin do
+    resources :restaurants      #設定後台路由
     root "restaurants#index"
   end
 end
