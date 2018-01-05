acdef06ecb612ad7ba430e641c6670cf4e109e8f
YuHsiang <b6100111524@hotmail.com>
2018-01-05 14:58:56 +0800

    set up admin authentication

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -1,4 +1,7 @@
 class Admin::RestaurantsController < ApplicationController
+  ## 在登入帳號前 進行身份驗證
+  before_action :authenticate_user!
+  before_action :authenticate_admin  
   ## 建立後台頁面
   def index
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  ## 判斷是否符合身份 role 字串是否符合身份
+  def admin?
+    self.role == "admin"
+  end
+
 end
