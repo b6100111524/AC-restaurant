695bbc3affb976e0dc31bd5dd9bda7f9ef663fb5
YuHsiang <b6100111524@hotmail.com>
2018-01-06 18:15:52 +0800

    add edit & update actions and view for admin

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -1,7 +1,9 @@
 class Admin::RestaurantsController < ApplicationController
   ## 在登入帳號前 進行身份驗證
   before_action :authenticate_user!
-  before_action :authenticate_admin  
+  before_action :authenticate_admin
+  ## 把私有方法限定給 show, edit & update
+  before_action :set_restaurant, only: [:show, :edit, :update]
   ## 建立後台 index 動作
   def index
     @restaurants = Restaurant.all
@@ -22,16 +24,30 @@ class Admin::RestaurantsController < ApplicationController
       render :new                                                 # 留在 new 頁面
     end
   end
-  ## 建立後台 show 動作  
-  def show
-    @restaurant = Restaurant.find(params[:id])
+  ## 建立後台 show & edit 動作 游絲有方法 set_restaurant 取代 
+  ## def show; end
+  ## def edit; end
+  ## 建立後台 update 動作
+  def update
+    if @restaurant.update(restaurant_params)
+      flash[:notice] = "Restaurant was successfully updated!"
+      redirect_to admin_restaurant_path(@restaurant)
+    else
+      flash.now[:alert] = "Restaurant was failed to update!"
+      render :edit
+    end
   end
 
 
 
+  ## 私有方法
   private
   ## 資安驗證
   def restaurant_params
     params.require(:restaurant).permit(:name, :opening_hour, :tel_on, :address, :description)
+  end
+  ##
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
   end
 end
