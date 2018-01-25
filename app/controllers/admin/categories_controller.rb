17d0b2f08040623230b7b244d9fb9d61972659ad
YuHsiang <b6100111524@hotmail.com>
2018-01-25 23:22:21 +0800

    user can delete category with link on index page

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -2,7 +2,6 @@ class Admin::CategoriesController < ApplicationController
   ## 身份驗證程序
   before_action :authenticate_user!  # 驗證請求進入後台的是否為已登入的 User
   before_action :authenticate_admin  # 驗證該 User 身份是否為網站管理員
-
   ## 類別頁面展示 index 動作
   def index
     @categories = Category.all
@@ -14,7 +13,6 @@ class Admin::CategoriesController < ApplicationController
       @category = Category.new
     end
   end
-
   ## 創造新的類別 create 動作
   def create
     @category = Category.new(category_params)
@@ -26,7 +24,6 @@ class Admin::CategoriesController < ApplicationController
       render :index
     end
   end
-
   ## 上傳修改的類別 update 動作
   def update
     @category = Category.find(params[:id])
@@ -38,7 +35,13 @@ class Admin::CategoriesController < ApplicationController
       render :index
     end
   end
-
+  ## 刪除類別 destory 動作
+  def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+    flash[:alert] = "category was successfully deleted"
+    redirect_to admin_categories_path
+  end
 
 
   private
