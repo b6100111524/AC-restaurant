a76e8b551b5bce002b3e9ca88d6fea7ecdb8de61
YuHsiang <b6100111524@hotmail.com>
2018-01-25 20:46:07 +0800

    add category index page for admin

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -2,4 +2,10 @@ class Admin::CategoriesController < ApplicationController
   ## 身份驗證程序
   before_action :authenticate_user!  # 驗證請求進入後台的是否為已登入的 User
   before_action :authenticate_admin  # 驗證該 User 身份是否為網站管理員
+
+  def index
+    @categories = Category.all
+  end
+
+
 end
