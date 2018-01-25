26d8b0187b8b8a655db20ebc823dcffaa16a37f2
YuHsiang <b6100111524@hotmail.com>
2018-01-25 20:23:58 +0800

    setup route resources for admin categories

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   # 後台路由設定 建立admin管理權限 (使用前置字元定義)
   namespace :admin do
     resources :restaurants      #設定後台路由
+    resources :categories       #設定分類路由
     root "restaurants#index"
   end
 end
