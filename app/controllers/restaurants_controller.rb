cf11681e6e4e15cea127cec43c01910acc0d5ba9
YuHsiang <b6100111524@hotmail.com>
2018-01-28 17:45:56 +0800

    add show page for public users

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,9 +2,13 @@ class RestaurantsController < ApplicationController
   ## 建立驗證程序
   before_action :authenticate_user!
   
-  ## 建立前台頁面
+  ## 建立前台頁面 index 動作
   def index
     @restaurants = Restaurant.page(params[:page]).per(9)
+  end
+  ## 建立展示頁面 show 動作
+  def show
+    @restaurant = Restaurant.find(params[:id])
   end
 
 end
