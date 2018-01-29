434d4f56a256bfb0e4be10ac85192bb5a8958814
YuHsiang <b6100111524@hotmail.com>
2018-01-29 23:51:28 +0800

    user can add comment on restaurant show page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,22 @@
 class CommentsController < ApplicationController
+  ## 建立 評論
+  def create
+    # 確認目標餐廳
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    # 建立一則評論 內容必須通過私有驗證 build = new
+    @comment = @restaurant.comments.build(comment_params)
+    # 確認使用者
+    @comment.user = current_user
+    # 儲存評論內容
+    @comment.save!
+    # 重新導向 show 更新頁面
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
+
 end
--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -7,9 +7,10 @@ class RestaurantsController < ApplicationController
     @restaurants = Restaurant.page(params[:page]).per(12)
     @categories = Category.all 
   end
-  ## 建立展示頁面 show 動作
+  ## 建立展示頁面 show 動作 + 評論
   def show
     @restaurant = Restaurant.find(params[:id])
+    @comment = Comment.new
   end
 
 end
