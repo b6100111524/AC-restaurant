44d4d4ece90636958dc4efac322eb660717f7d98
YuHsiang <b6100111524@hotmail.com>
2018-02-03 17:35:03 +0800

    user can update the profile detailes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,30 @@
 class UsersController < ApplicationController
+  ## 共用私有指令 在執行特定 action 時先執行
+  before_action :set_user, only: [:show, :edit, :update]
 
-  ## 建立 使用者 頁面展示
+  ## 建立 使用者 展示功能
   def show
-    @user = User.find(params[:id])
+  end
+  ## 建立 使用者 編輯功能
+  def edit
+    unless @user == current_user
+      redirect_to user_path(@user)
+    end
+  end
+  ## 建立 使用者 更新功能
+  def update
+    @user.update(user_params)
+    redirect_to user_path(@user)
   end
 
+  ## 私有指令
+  private
+  ## 讀取 使用者 內容
+  def set_user
+    @user = User.find(params[:id])
+  end
+  ## 驗證程序
+  def user_params
+    params.require(:user).permit(:name, :intro)
+  end
 end
